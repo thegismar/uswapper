549040867122345f077f6cfbb3152fdc7f76677e
thegismar <thegismar@gmail.com>
2020-08-07 05:34:54 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='uswapper',
-      version='1.2',
+      version='1.3',
       description='Simple wrapper for uniswap graphql api',
       license='MIT',
       packages=['uswapper'],
